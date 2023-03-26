d56085a432a0b95ce9e081ff671c35e9eee87ed3
CANALERO12 <santiagobuentipo@gmail.com>
2023-03-25 23:59:02 -0500

    nueva funcion

--- src/pratica.java
+++ src/pratica.java
@@ -3,7 +3,7 @@ public class pratica {
 
 
     public static void main(String[] args) {
-        int[] arr={1,2,4,20,3,9,40};
+        int[] arr={2,4,20,3,40,9,-5};
         int max= arr[0];
         for(int i=1; i< arr.length; i++){
             if(arr[i] > max){
@@ -12,5 +12,13 @@ public class pratica {
             }
         }
         System.out.println("El numero mas grande es : " + max);
+        int min= arr[0];
+        for(int i=1; i< arr.length; i++){
+            if(arr[i] < min){
+                min=arr[i];
+
+            }
+        }
+        System.out.println("El nuero mas pequeño es : " + min);
     }
 }
